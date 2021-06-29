259ef24a3db0e418353aa993c8c1db28ffc60a3f
Naoki Hamada <hmkz@s101.xrea.com>
2021-06-29 14:31:30 +0900

    Create .nojekyll (#5)
    * Create .nojekyll
    
    * Rename doc/.nojekyll to doc/_build/html/.nojekyll
    
    * Delete .nojekyll
    
    * Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -33,6 +33,7 @@ release = '0.0.1'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
+    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
