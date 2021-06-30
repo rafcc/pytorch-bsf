d80cccb1089c483ebe2194e5a8b8c091f81ad71c
Naoki Hamada <hmkz@s101.xrea.com>
2021-06-30 22:47:39 +0900

    Docstring (#11)
    * Update docstring
    
    * Update docstring
    
    * Update docstring
    
    * Update docstring
    
    * Update docstring
    
    * Update docstring
    
    * Update conf.py
    
    * Fix URL
    
    * Show lines
    
    * Read another requirements.txt
    
    * Remove unnecessary packages
    
    * Use numpydoc
    
    * Add numpydoc in requirements.txt
    
    * Update docstring
    
    * Update docstring
    
    * Add doctest
    
    * Add doctest_global_setup
    
    * Add blank lines at the end of definitions
    
    * Add doctest

--- torch_bsf/bezier_simplex.py
+++ torch_bsf/bezier_simplex.py
@@ -11,6 +11,27 @@ from torch.utils.data import DataLoader, TensorDataset, random_split
 
 
 class BezierSimplexDataModule(pl.LightningDataModule):
+    """A data module for training a Bezier simplex.
+
+    Parameters
+    ----------
+    data
+        The path to a data file.
+    label
+        The path to a label file.
+    header
+        The number of headers in data files.
+    delimiter
+        The delimiter of data files.
+    batch_size
+        The size of minibatch.
+    split_ratio
+        The ratio of train-val split.
+    normalize
+        The data normalization method.
+        Either `"max"`, `"std"`, `"quantile"`, or `"none"`.
+
+    """
     def __init__(
         self,
         data: str,
@@ -90,6 +111,21 @@ class BezierSimplexDataModule(pl.LightningDataModule):
 Index = Tuple[int, ...]
 
 def indices(dim: int, deg: int) -> Iterable[Index]:
+    """Iterates the index of control points of the Bezier simplex.
+
+    Parameters
+    ----------
+    dim
+        The array length of indices.
+    deg
+        The degree of the Bezier simplex.
+
+    Returns
+    -------
+    indices
+        The indices.
+
+    """
     def iterate(c, r):
         if len(c) == dim - 1:
             yield c + (r, )
@@ -102,6 +138,21 @@ def indices(dim: int, deg: int) -> Iterable[Index]:
 
 @lru_cache(1024)
 def polynom(degree: int, index: Iterable[int]) -> float:
+    """Computes a polynomial coefficient.
+
+    Parameters
+    ----------
+    degree
+        The degree.
+    index
+        The index.
+
+    Returns
+    -------
+    polynom
+        The polynomial coefficient.
+
+    """
     r = factorial(degree)
     for i in index:
         r /= factorial(i)
@@ -109,12 +160,70 @@ def polynom(degree: int, index: Iterable[int]) -> float:
 
 
 def monomial(var: Iterable[float], deg: Iterable[int]) -> torch.Tensor:
+    """Computes a monomial `var**deg = v[0]**d[0] * v[1]**d[1] * ... * v[n]**d[n]`.
+
+    Parameters
+    ----------
+    var
+        The bases.
+    deg
+        The powers.
+    
+    Returns
+    -------
+    monomial
+        The monomial.
+
+    """
     var = torch.as_tensor(var)
     deg = torch.as_tensor(deg, device=var.device)
     return (var ** deg).prod(dim=-1)
 
 
 class BezierSimplex(pl.LightningModule):
+    """A Bezier simplex model.
+
+    Parameters
+    ----------
+    n_params
+        The number of parameters.
+    n_values
+        The number of values.
+    degree
+        The degree of the Bezier simplex.
+    
+    Examples
+    --------
+    >>> ts = torch.tensor(  # parameters on a simplex
+    ...     [
+    ...         [3/3, 0/3, 0/3],
+    ...         [2/3, 1/3, 0/3],
+    ...         [2/3, 0/3, 1/3],
+    ...         [1/3, 2/3, 0/3],
+    ...         [1/3, 1/3, 1/3],
+    ...         [1/3, 0/3, 2/3],
+    ...         [0/3, 3/3, 0/3],
+    ...         [0/3, 2/3, 1/3],
+    ...         [0/3, 1/3, 2/3],
+    ...         [0/3, 0/3, 3/3],
+    ...     ]
+    ... )
+    >>> xs = 1 - ts * ts  # values corresponding to the parameters
+    >>> dl = DataLoader(TensorDataset(ts, xs))
+    >>> bs = BezierSimplex(
+    ...     n_params=int(ts.shape[1]),
+    ...     n_values=int(xs.shape[1]),
+    ...     degree=3,
+    ... )
+    >>> trainer = pl.Trainer(
+    ...     gpus=0,
+    ...     max_epochs=10,
+    ...     callbacks=[EarlyStopping(monitor="val_mse")],
+    ... )
+    >>> trainer.fit(bs, dl)
+    >>> ts, xs = bs.meshgrid()
+
+    """
     def __init__(
         self,
         n_params: int,
@@ -131,6 +240,19 @@ class BezierSimplex(pl.LightningModule):
         })
 
     def forward(self, t: torch.Tensor) -> torch.Tensor:
+        """Process a forwarding step of training.
+
+        Parameters
+        ----------
+        t
+            A minibatch of parameter vectors.
+
+        Returns
+        -------
+        x
+            A minibatch of value vectors. 
+
+        """
         # REQUIRED
         x = 0
         for i in indices(self.n_params, self.degree):
@@ -179,19 +301,20 @@ class BezierSimplex(pl.LightningModule):
         return optimizer
  
     def meshgrid(self, num: int = 100) -> Tuple[torch.Tensor, torch.Tensor]:
-        """Meshgrid
+        """Computes a meshgrid of the Bezier simplex.
 
         Parameters
         ----------
         num
-            number of grid points on each edge
+            The number of grid points on each edge.
 
         Returns
         ----------
-        ts : torch.Tensor
-            Array of mesh grid
-        xs : torch.Tensor
-            The concatenated array
+        ts
+            A parameter matrix of the mesh grid.
+        xs
+            A value matrix of the mesh grid.
+
         """
         ts = torch.Tensor(list(indices(dim=self.n_params, deg=num))) / num
         xs = self.forward(ts)
@@ -207,7 +330,64 @@ def fit(
     gpus: Union[str, int, List[int]]=-1,
     num_nodes: int=1,
     accelerator: str="ddp",
-):
+) -> BezierSimplex:
+    """Fits a Bezier simplex.
+    
+    Parameters
+    ----------
+    params
+        The data.
+    values
+        The label data.
+    degree
+        The degree of the Bezier simplex.
+    batch_size
+        The size of minibatch.
+    max_epochs
+        The number of epochs to stop training.
+    gpus
+        The number of gpus.
+    num_nodes
+        The number of compute nodes.
+    accelerator
+        Distributed mode.
+    
+    Returns
+    -------
+    bs
+        A trained Bezier simplex.
+    
+    Examples
+    --------
+    >>> import torch
+    >>> import torch_bsf
+
+    Prepare training data
+    >>> ts = torch.tensor(  # parameters on a simplex
+    ...     [
+    ...         [3/3, 0/3, 0/3],
+    ...         [2/3, 1/3, 0/3],
+    ...         [2/3, 0/3, 1/3],
+    ...         [1/3, 2/3, 0/3],
+    ...         [1/3, 1/3, 1/3],
+    ...         [1/3, 0/3, 2/3],
+    ...         [0/3, 3/3, 0/3],
+    ...         [0/3, 2/3, 1/3],
+    ...         [0/3, 1/3, 2/3],
+    ...         [0/3, 0/3, 3/3],
+    ...     ]
+    ... )
+    >>> xs = 1 - ts * ts  # values corresponding to the parameters
+
+    Train a model
+    >>> bs = torch_bsf.fit(params=ts, values=xs, degree=3, max_epochs=100)
+
+    Predict by the trained model
+    >>> t = [[0.2, 0.3, 0.5]]
+    >>> x = bs(t)
+    >>> print(f"{t} -> {x}")
+
+    """
     data = TensorDataset(params, values)
     dl = DataLoader(data, batch_size=batch_size or len(data))
     bs = BezierSimplex(n_params=int(params.shape[1]), n_values=int(values.shape[1]), degree=degree)
