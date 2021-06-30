db4f314d0a5dc27df8d079e887c2ef9aa0dace39
Hamada Naoki <hmkz@s101.xrea.com>
2021-06-30 20:47:42 +0900

    Use numpydoc

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.githubpages',
     'sphinx.ext.linkcode',
-    'sphinx.ext.napoleon',
+    'numpydoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
